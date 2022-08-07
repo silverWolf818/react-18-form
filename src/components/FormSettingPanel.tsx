37a9bb0621c210dccc827d0dbe3b4f48826ace16
zzh <zhouzhihao@jinshuju.net>
2022-08-07 13:24:46 +0800

    dev

--- src/components/FormSettingPanel.tsx
+++ src/components/FormSettingPanel.tsx
@@ -1,5 +1,20 @@
-const FormSettingPanel = ()=> {
-    return <div className="form-setting-panel"></div>
-}
+import {onFieldInitialValueChange, onFieldReact, onFieldValueChange} from '@formily/core'
+import {observer, useFormEffects} from '@formily/react'
+
+const FormSettingPanel = observer(() => {
+    useFormEffects(() => {
+        onFieldReact('fields', (field) => {
+            // @ts-ignore
+            console.log(JSON.stringify(field.value))
+        })
+        // onFieldInitialValueChange('fields', (field) => {
+        //     console.log(field.value)
+        // })
+        // onFieldValueChange('fields', (field) => {
+        //     console.log(field.value)
+        // })
+    })
+    return <div className="form-setting-panel">1</div>
+})
 
 export default FormSettingPanel
---
+++ src/hooks/useFieldEffect.tsx
@@ -0,0 +1,5 @@
+const useFieldEffect = () => {
+    return null
+}
+
+export default useFieldEffect
